94e6111494959f8147dc6ac0424b416aef7f0914
Miguel Ángel <108587986+miguelangelgmm@users.noreply.github.com>
2023-01-27 12:36:57 +0100

    añadida toda la funcionalidad principal

--- js/app.js
+++ js/app.js
@@ -1,30 +1,32 @@
+
 // Variables globales
 let area;
 let cube;
 let acctions = [];
+let deleted;
 
 let size = 50;
 
 let position = document.getElementById("position");
 
+let color = { r: 255, g: 0, b: 0 };
+let coordinates = { x: 0, y: 0 };
+
+let cubes = new Map();
+let cubesDel = [];
+deleted = document.getElementById("deleted");
 
 // Área para el proyecto
 let main = document.getElementsByTagName("main")[0];
 area = document.createElement("div");
 area.classList.add("container");
-area.style.border = "2px solid red";
-area.style.height = "400px";
-area.style.position = "relative";
 main.parentElement.insertBefore(area, main);
+
 
 // Pieza que queremos mover
 cube = document.createElement("div");
-cube.style.background = "red";
-cube.style.width = `${size}px`;
-cube.style.height = `${size}px`;
-cube.style.position = "absolute";
-cube.style.top = "100px";
-cube.style.left = "150px";
+cube.style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
+cube.classList.add("cube");
 area.appendChild(cube);
 
 
@@ -47,14 +49,14 @@ document.addEventListener("keydown", function (event) {
 		case "KeyC":
 			addAction("color");
 			break;
-		case "Enter":
-			executeAcctions();
-			break;
 		case "NumpadAdd":
-			addSize();
+			addAction("+");
 			break;
 		case "NumpadSubtract":
-			subtractSize();
+			addAction("-");
+			break;
+		case "Enter":
+			executeAcctions();
 			break;
 		default:
 			break;
@@ -92,10 +94,10 @@ function moveRight(cube) {
 }
 
 function randomColor(cube) {
-	let r = Math.floor((Math.random() * 256));
-	let g = Math.floor((Math.random() * 256));
-	let b = Math.floor((Math.random() * 256));
-	cube.style.background = `rgb(${r}, ${g}, ${b})`;
+	color.r = Math.floor((Math.random() * 256));
+	color.g = Math.floor((Math.random() * 256));
+	color.b = Math.floor((Math.random() * 256));
+	cube.style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
 }
 
 // Registro de acción y generación de span
@@ -106,12 +108,7 @@ function addAction(action) {
 		span: span
 	});
 	span.textContent = action;
-	span.style.padding = "10px";
-	span.style.border = "1px solid #ddd";
-	span.style.display = "block";
-	span.style.float = "left";
-	span.style.margin = "2px";
-	span.style.cursor = "pointer";
+	//Los estilos del span estan en la css
 	span.addEventListener("mouseenter", function () {
 		this.style.backgroundColor = "red";
 		this.style.color = "white";
@@ -120,12 +117,13 @@ function addAction(action) {
 		this.style.backgroundColor = "white";
 		this.style.color = "black";
 	})
-	span.addEventListener("click", function () {
+	span.addEventListener("click", function (event) {
 		let index = acctions.findIndex((action) => {
 			return action.span === this;
 		})
 		acctions.splice(index, 1);
 		this.remove();
+		event.stopPropagation()
 	})
 	area.appendChild(span);
 }
@@ -150,6 +148,12 @@ function executeAcctions() {
 			case "color":
 				randomColor(cube);
 				break;
+			case "+":
+				addSize();
+				break;
+			case "-":
+				subtractSize();
+				break;
 			default:
 				break;
 		}
@@ -158,20 +162,31 @@ function executeAcctions() {
 	}
 }
 
-
-
 area.addEventListener("mousemove", (event) => {
-    //solo tenemos en cuentan el cuadrado de area
-    if (event.currentTarget === area) {
-        let x = event.clientX;
-        let y = event.clientY;
-        this.position.innerHTML = `X: ${x} Y: ${y}`;
-    }
+	/*
+		Si uso event.offset cuando me muevo y estoy en un cube
+		se activa el evento y me pone el offset en función a ese cubo,
+		para poder eliminar este problema he tenido que usar la función
+		getBoundingClientRect
+		Me devuelve los datos de las posiciones del cuadrado de area, su ancho y su alto
+		x: 8, y: 8, width: 925, height: 404, top: 8, right: 933, bottom: 412, left: 8 }
+
+		después he tomado la posición de las coordenas con event.client y le resto la separación en top y left
+		coordinates.x = event.offsetX; --> coordinates.x = event.clientX - rect.left;
+		coordinates.y = event.offsetY; --> coordinates.y = event.clientY - rect.top;
+	*/
+
+	let rect = area.getBoundingClientRect();
+	coordinates.x = event.clientX - rect.left;
+	coordinates.y = event.clientY - rect.top;
+
+	this.position.innerHTML = `X: ${coordinates.x} Y: ${coordinates.y}`;
+
 });
 
 //Reseteamos las posiciones
 area.addEventListener("mouseleave", (event) => {
-    this.position.innerHTML = `X: 0 Y: 0`;
+	this.position.innerHTML = `X: 0 Y: 0`;
 });
 
 function addSize() {
@@ -189,9 +204,84 @@ function addSize() {
 
 }
 function subtractSize() {
-	if (size - 5 > 10) {
+	if (size - 5 >= 10) {
 		size -= 5;
 		cube.style.width = `${size}px`;
 		cube.style.height = `${size}px`;
 	}
+}
+let count = 0;
+
+let removeCubesEvent = new CustomEvent("removeCubes", {
+	bubbles: true,
+	detail: {
+		index: 0
+	}
+});
+
+area.addEventListener("click", (event) => {
+	//Solo se activa en area, si hago click en el primer cuadrado que no se puede eliminar no hace nada
+	if (event.target == event.currentTarget) {
+		let cube = document.createElement("div");
+		let span = document.createElement("span");
+		let index = ++count;
+
+		//propiedades del cubo
+		cube.style.background = `rgb(${color.r}, ${color.g}, ${color.b})`;
+		cube.classList.add("cubeCursor", "cube");
+		cube.style.top = `${coordinates.y}px`;
+		cube.style.left = `${coordinates.x}px`;
+		//propiedades del span
+		span.classList.add("indexCube");
+		span.textContent = index;
+
+		//aumentamos el mapa de cubes
+		cubes.set(index, { cube: cube, span: span });
+
+		//añadimos el evento para hacer click y eliminarlo
+		cube.addEventListener("click", function (event) {
+			//lanzamos el evento personalizado
+			let pos = parseInt(cube.textContent)
+			removeCubesEvent.detail.index = pos;
+			area.dispatchEvent(removeCubesEvent);
+
+			event.stopPropagation()
+
+		})
+
+		//añadimos a area y a cube
+		area.appendChild(cube);
+		cube.appendChild(span);
+	}
+})
+
+
+area.addEventListener("removeCubes", function (event) {
+	let index = event.detail.index
+	let cube = cubes.get(index);
+	cubesDel.push(cube);
+	cube.cube.classList.remove("cubeCursor", "cube");
+	let color = cube.cube.style.background;
+
+	cube.cube.removeAttribute("style");
+	cube.cube.classList.add("cubeDel");
+
+	cube.cube.style.background = color;
+
+	cubes.delete(index);
+	cubesDel.sort((a, b) => { return parseInt(a.cube.textContent) - parseInt(b.cube.textContent) })
+
+	newPos = searchCube(cube.cube);
+
+	if (cubesDel.length - 1 == newPos) {
+		deleted.appendChild(cubesDel[cubesDel.length - 1].cube);
+	}
+	else {
+		deleted.insertBefore(cubesDel[newPos].cube, cubesDel[newPos + 1].cube);
+	}
+
+})
+
+function searchCube(cube) {
+	return cubesDel.findIndex((cub) => cub.cube.textContent == cube.textContent);
 }
